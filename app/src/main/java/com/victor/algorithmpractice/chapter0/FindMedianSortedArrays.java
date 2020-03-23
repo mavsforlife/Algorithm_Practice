a099c01916d40880e3d2b791b6f982504f1a495d
Victor <wangweimin@bilibili.com>
2020-03-23 22:34:54 +0800

    find median sorted arrays
    --user Victor

---
+++ app/src/main/java/com/victor/algorithmpractice/chapter0/FindMedianSortedArrays.java
@@ -0,0 +1,85 @@
+package com.victor.algorithmpractice.chapter0;
+
+/**
+ * @author victor
+ * @Description 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * <p>
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * <p>
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * <p>
+ * 示例 1:
+ * <p>
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * <p>
+ * 则中位数是 2.0
+ * 示例 2:
+ * <p>
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * <p>
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * <p>
+ * No.4
+ * @since 2020-03-23
+ */
+public class FindMedianSortedArrays {
+
+    private static double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) { // to ensure m<=n
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {
+                iMin = i + 1; // i is too small
+            } else if (i > iMin && A[i - 1] > B[j]) {
+                iMax = i - 1; // i is too big
+            } else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("nums1 = [1, 3]\n" + "nums2 = [2]");
+        double double1 = findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
+        System.out.println("median num = " + double1);
+
+        System.out.println("nums1 = [1, 2]\n" + "nums2 = [3, 4]");
+        double double2 = findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4});
+        System.out.println("median num = " + double2);
+    }
+}
